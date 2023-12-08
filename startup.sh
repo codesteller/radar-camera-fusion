25ecac6b6bf331708cd9873dbce6d08ff420b94d
g-codesteller <pallab.maji@gahanai.com>
2023-12-08 15:20:32 +0530

    fixed dockerfile

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+device="$1"
 docker build -f docker/Dockerfile -t docker-swiftly:latest .
 docker run -it --rm --gpus all -v ${PWD}:/workspace/swiftly/ --net=host --ipc=host \
-    -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/video4 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device ${device} \
     -w /workspace/swiftly/build docker-swiftly:latest \
     ./adas_fusion ../assets/models/yolov8s.engine /dev/video4
